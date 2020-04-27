45f3a9d07ee5565d7ffc6aaebd67edba5153e807
Elias Ojeda Medina <eliasojedamedina@gmail.com>
2020-04-27 00:34:17 -0500

    :rocket: Update Admin

--- PersonalPage/urls.py
+++ PersonalPage/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
@@ -25,3 +26,9 @@ urlpatterns = [
     path('portfolio/', views.portfolio, name="portfolio"),
     path('contact/', views.contact, name="contact")
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- portfolio/admin.py
+++ portfolio/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from portfolio.models import Project
 
-# Register your models here.
-admin.site.register(Project)
+
+class ProjectAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+
+admin.site.register(Project, ProjectAdmin)
